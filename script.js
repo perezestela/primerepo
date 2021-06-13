6f8af8e1d8d32dde5e27952272c7b5e0ff9b9277
perezestela <85747702+perezestela@users.noreply.github.com>
2021-06-12 22:31:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-var edad= parseInt(prompt('Ingrese su edad')) 
-console.log(edad)
-if (edad>=18){
-    alert('Es mayor de edad')
-}else{
-    alert('No es mayor de edad')
+var mayor=0
+
+function evaluar(nombre,edad){
+    console.log(nombre, edad)
+    if (edad>=18){
+        alert(nombre+' SI es mayor de edad')
+        if (edad>mayor){
+            mayor=edad
+            usuario=nombre
+        }
+    }else{
+         alert(nombre+' NO es mayor de edad')
+    }
 }
+for(i=1;i<4;i++){
+    var nombre= prompt('Ingrese el nombre del usuario '+i)
+    var edad= parseInt(prompt('De '+nombre+ ' ingrese su edad')) 
+    evaluar(nombre,edad)
+}
+alert('De los tres usuarios ingresados '+ usuario + ' es el/la mayor y su edad es ' + mayor+' años.')
